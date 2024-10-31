3ab91c3050afe7b56234eda67a0254834695faa0
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2024-10-30 22:40:36 -0300

    feat: create theme toogle and user menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme/theme-provider';
 import './global.css';
 import { router } from './routes';
 
 export const App = () => {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop.dashboard" />
-      <RouterProvider router={router} />
-      <Toaster richColors closeButton />
+      <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
+        <Helmet titleTemplate="%s | pizza.shop.dashboard" />
+        <RouterProvider router={router} />
+        <Toaster richColors closeButton />
+      </ThemeProvider>
     </HelmetProvider>
   );
 };
